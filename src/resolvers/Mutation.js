967a1726759438e446c1c1a90810743cf097d30e
addyvan <avandenhoeve14@ubishops.ca>
2018-11-26 13:28:50 -0500

    Added mutations to add members to a group

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -50,13 +50,12 @@ function createPost(_, args, context, info) {
         content: args.content
     };
 
-    console.log(createPostData);
-
     sendMessageToRabbitMQ('create', JSON.stringify(createPostData));
 
     return context.prisma.mutation.createPost({
         data: createPostData,
         }, info);
+
 }
 
 async function deletePost(_, args, context, info) {
@@ -70,9 +69,87 @@ async function deletePost(_, args, context, info) {
 
 }
 
+async function addUserToGroup(_, args, context, info) {
+
+    var updateUserData = {};
+    var updateGroupData = {};
+
+    const currentUser = await context.prisma.query.user(
+        {
+            where: {
+                username: args.user.username
+            }            
+        });
+    
+    if (currentUser == null | undefined){
+        throw new Error(`Could not find profile with gcId ${args.user.username}`)
+    }
+
+    const currentGroup = await context.prisma.query.group(
+        {
+            where: {
+                name: args.group.name
+            }            
+        });
+
+    if (currentGroup == null | undefined){
+        throw new Error(`Could not find profile with gcId ${args.group.name}`)
+    }
+
+    // add group to user's subscribed groups
+    if (currentUser.groups) {
+        updateUserData.groups = currentUser.groups;
+        updateUserData.groups.push({
+            connect: {
+                name: currentGroup.name
+            }
+        }); 
+    } else {
+        updateUserData.groups = {
+            connect: {
+                name: currentGroup.name
+            }
+        }
+    }
+
+    // add user to group's members list
+    if (currentGroup.members) {
+        updateGroupData.groups = currentGroup.members;
+        updateGroupData.members.push({
+            connect: {
+                username: currentUser.username
+            }
+        }); 
+    } else {
+        updateGroupData.members = {
+            connect: {
+                username: currentUser.username
+            }
+        }
+    }
+    
+    await context.prisma.mutation.updateGroup({
+        where: {
+            name: args.group.name
+        },
+        data: updateGroupData
+    });
+
+    return await context.prisma.mutation.updateUser({
+        where:{
+            username: args.user.username
+        },
+        data: updateUserData
+    });
+
+}
+
+
+
 module.exports = {
     createUser,
     createGroup,
     createPost,
-    deletePost
+    deletePost,
+    addUserToGroup
 };
