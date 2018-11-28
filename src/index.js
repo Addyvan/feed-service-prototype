d7f657453e1cf892c037bc5158c6ac421a79833d
addyvan <avandenhoeve14@ubishops.ca>
2018-11-28 10:57:16 -0500

    chnaged name to feed

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const server = new GraphQLServer({
     ...req,
     prisma: new Prisma({
       typeDefs: './src/generated/prisma.graphql',
-      endpoint: 'http://localhost:4466/profile/',
+      endpoint: 'http://localhost:4466/feed/',
       debug: true,
     }),
   }),
